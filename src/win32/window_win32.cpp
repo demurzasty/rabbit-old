85023b0e4efbfea600e81c4f9f73b8430794d75a
Cyste <thecyste@gmail.com>
2020-11-16 23:22:44 +0100

    cleanup window win32 implementation

--- src/win32/window_win32.cpp
+++ src/win32/window_win32.cpp
@@ -98,95 +98,10 @@ LRESULT CALLBACK window_proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
 		SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)((CREATESTRUCT*)(lParam))->lpCreateParams);
 	}
 
-#if RB_EDITOR
-	ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam);
-#endif
-
 	window_win32* window = hwnd ? reinterpret_cast<window_win32*>(GetWindowLongPtr(hwnd, GWLP_USERDATA)) : NULL;
 	if (window) {
-		switch (msg) {
-			case WM_CLOSE:
-				window->close();
-				break;
-			case WM_SETCURSOR:
-				if (LOWORD(lParam) == HTCLIENT) {
-					SetCursor(LoadCursor(NULL, IDC_ARROW));
-				}
-				break;
-			case WM_SIZE:
-				//if (service_collection::has<graphics_device>()) {
-				//	service_collection::get<graphics_device>().set_backbuffer_size({ LOWORD(lParam), HIWORD(lParam) });
-				//}
-				//if (has_plugin<graphics>()) {
-				//	get_plugin<graphics, graphics_directx>().set_backbuffer_size({ LOWORD(lParam), HIWORD(lParam) });
-				//}
-				break;
-			case WM_LBUTTONUP:
-			case WM_LBUTTONDOWN:
-				// detail::set_mouse_button(mouse_button::left, msg == WM_LBUTTONDOWN);
-				break;
-			case WM_MBUTTONUP:
-			case WM_MBUTTONDOWN:
-				// detail::set_mouse_button(mouse_button::middle, msg == WM_MBUTTONDOWN);
-				break;
-			case WM_RBUTTONUP:
-			case WM_RBUTTONDOWN:
-				// detail::set_mouse_button(mouse_button::right, msg == WM_RBUTTONDOWN);
-				break;
-			case WM_MOUSEMOVE:
-			{
-				const auto x = static_cast<std::int16_t>(LOWORD(lParam));
-				const auto y = static_cast<std::int16_t>(HIWORD(lParam));
-
-				// detail::set_mouse_position(vec2i(x, y));
-				break;
-			}
-			case WM_MOUSEWHEEL:
-			{
-				const auto delta = static_cast<std::int16_t>(HIWORD(wParam));
-				// detail::set_mouse_wheel(delta / 120.0f);
-				break;
-			}
-			case WM_CHAR:
-			{
-				const auto codepoint = static_cast<std::uint32_t>(wParam);
-
-				char text[5] = { 0 };
-				if (codepoint <= 0x7F) {
-					text[0] = (char)codepoint;
-					text[1] = '\0';
-				} else if (codepoint <= 0x7FF) {
-					text[0] = 0xC0 | (char)((codepoint >> 6) & 0x1F);
-					text[1] = 0x80 | (char)(codepoint & 0x3F);
-					text[2] = '\0';
-				} else if (codepoint <= 0xFFFF) {
-					text[0] = 0xE0 | (char)((codepoint >> 12) & 0x0F);
-					text[1] = 0x80 | (char)((codepoint >> 6) & 0x3F);
-					text[2] = 0x80 | (char)(codepoint & 0x3F);
-					text[3] = '\0';
-				} else if (codepoint <= 0x10FFFF) {
-					text[0] = 0xF0 | (char)((codepoint >> 18) & 0x0F);
-					text[1] = 0x80 | (char)((codepoint >> 12) & 0x3F);
-					text[2] = 0x80 | (char)((codepoint >> 6) & 0x3F);
-					text[3] = 0x80 | (char)(codepoint & 0x3F);
-					text[4] = '\0';
-				}
-
-				if (codepoint >= 32 && text[0] != '\0') {
-					// detail::add_input_text(text);
-				}
-				break;
-			}
-			case WM_KEYDOWN:
-			case WM_SYSKEYDOWN:
-				if ((HIWORD(lParam) & KF_REPEAT) == 0) {
-					// detail::set_key(translate_key(wParam, lParam), true);
-				}
-				break;
-			case WM_KEYUP:
-			case WM_SYSKEYUP:
-				// detail::set_key(translate_key(wParam, lParam), false);
-				break;
+		if (msg = WM_CLOSE) {
+			window->close();
 		}
 	}
 
