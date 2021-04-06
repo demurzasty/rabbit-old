200156e1ee7a13652b79431d174c9da5421a0788
demurzasty <thecyste@gmail.com>
2021-04-06 21:44:33 +0200

    integrate entt library

---
+++ include/rabbit/meta.hpp
@@ -0,0 +1,10 @@
+#pragma once 
+
+#include <entt/meta/meta.hpp>
+#include <entt/meta/factory.hpp>
+#include <entt/meta/resolve.hpp>
+
+namespace rb {
+    using entt::meta;
+    using entt::resolve;
+}
