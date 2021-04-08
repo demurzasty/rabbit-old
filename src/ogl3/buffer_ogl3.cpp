0cb0940d99f1af97ac04775351f5a836af3d937e
demurzasty <thecyste@gmail.com>
2021-04-08 14:12:41 +0200

    dsa implementation of opengl buffer

--- src/ogl3/buffer_ogl3.cpp
+++ src/ogl3/buffer_ogl3.cpp
@@ -15,16 +15,8 @@ static std::map<buffer_type, GLenum> types = {
 buffer_ogl3::buffer_ogl3(const buffer_desc& desc)
     : buffer(desc)
     , _type(types.at(desc.type)) {
-    glGenBuffers(1, &_id);
-    glBindBuffer(_type, _id);
-
-    if (desc.data) {
-        glBufferData(_type, desc.size, desc.data, desc.is_mutable ? GL_DYNAMIC_DRAW : GL_STATIC_DRAW);
-    } else {
-        glBufferData(_type, desc.size, 0, desc.is_mutable ? GL_DYNAMIC_DRAW : GL_STATIC_DRAW);
-    }
-
-    glBindBuffer(_type, 0);
+    glCreateBuffers(1, &_id);
+    glNamedBufferData(_id, desc.size, desc.data, desc.is_mutable ? GL_DYNAMIC_DRAW : GL_STATIC_DRAW);
 }
 
 buffer_ogl3::~buffer_ogl3() {
@@ -40,12 +32,11 @@ void* buffer_ogl3::map() {
         throw make_exception("[OGL3] Buffer need to be mutable to update content");
     }
 
-    glBindBuffer(_type, _id);
-    return glMapBuffer(_type, GL_READ_WRITE);
+    return glMapNamedBuffer(_id, GL_READ_WRITE);
 }
 
 void buffer_ogl3::unmap() {
-    glUnmapBuffer(_type);
+    glUnmapNamedBuffer(_id);
 }
 
 void buffer_ogl3::update(const void* data, std::size_t size) {
@@ -57,6 +48,5 @@ void buffer_ogl3::update(const void* data, std::size_t size) {
         throw make_exception("[OGL3] Overflow data");
     }
 
-    glBindBuffer(_type, _id);
-    glBufferSubData(_type, 0, size, data);
+    glNamedBufferSubData(_id, 0, size, data);
 }
