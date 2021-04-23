4247112024b9aa0406df401bdcf7b89620378ce0
demurzasty <thecyste@gmail.com>
2021-04-23 10:52:13 +0200

    add macro definition on graphics backend selection

--- src/graphics/graphics_device.cpp
+++ src/graphics/graphics_device.cpp
@@ -1,7 +1,9 @@
 #include <rabbit/graphics/graphics_device.hpp>
 
+#if RB_VULKAN
 #include "vulkan/graphics_device_vulkan.hpp"
 namespace rb { using graphics_device_impl = graphics_device_vulkan; }
+#endif
 
 using namespace rb;
 
