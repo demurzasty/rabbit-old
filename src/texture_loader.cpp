909656d0a47d3271fc22c7fa94dad864ff31278d
demurzasty <thecyste@gmail.com>
2021-07-23 18:33:14 +0200

    move graphics_device smart pointer in texture loader constructor

--- src/texture_loader.cpp
+++ src/texture_loader.cpp
@@ -1,16 +1,12 @@
 #include <rabbit/texture_loader.hpp>
 #include <rabbit/image.hpp>
-#include <rabbit/json.hpp>
 
-#include <map>
 #include <string>
-#include <fstream>
-#include <cassert>
 
 using namespace rb;
 
 texture_loader::texture_loader(std::shared_ptr<graphics_device> graphics_device)
-    : _graphics_device(graphics_device) {
+    : _graphics_device(std::move(graphics_device)) {
 }
 
 std::shared_ptr<void> texture_loader::load(const std::string& filename, const json& metadata) {
