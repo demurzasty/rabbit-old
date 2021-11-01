cc0eb8ce2aaf7f8e75466df73e97671cdb917286
demurzasty <thecyste@gmail.com>
2021-11-01 16:13:14 +0100

    add window borderless fullscreen

--- src/win32/window_win32.cpp
+++ src/win32/window_win32.cpp
@@ -73,8 +73,15 @@ window_win32::window_win32() {
 	RegisterClass(&wc);
 
 	HDC screenDC = GetDC(NULL);
-	const int left = (GetDeviceCaps(screenDC, HORZRES) - settings::window_size.x) / 2;
-	const int top = (GetDeviceCaps(screenDC, VERTRES) - settings::window_size.y) / 2;
+	auto screen_width = (unsigned int)GetDeviceCaps(screenDC, HORZRES);
+	auto screen_height = (unsigned int)GetDeviceCaps(screenDC, VERTRES);
+
+	if (settings::fullscreen) {
+		settings::window_size = { screen_width, screen_height };
+	}
+
+	auto left = (screen_width - settings::window_size.x) / 2;
+	auto top = (screen_height - settings::window_size.y) / 2;
 	ReleaseDC(NULL, screenDC);
 
 	RECT rect;
@@ -83,7 +90,7 @@ window_win32::window_win32() {
 	rect.bottom = settings::window_size.y;
 
 	const DWORD style = settings::fullscreen ?
-		WS_VISIBLE :
+		WS_VISIBLE | WS_POPUP : 
 		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU | WS_VISIBLE;
 
 	auto width = settings::window_size.x;
@@ -94,31 +101,16 @@ window_win32::window_win32() {
 			width = rect.right - rect.left;
 			height = rect.bottom - rect.top;
 		}
+	} else {
+		left = 0;
+		top = 0;
+		width = screen_width;
+		height = screen_height;
 	}
 
-	_hwnd = CreateWindowEx(WS_EX_APPWINDOW | WS_EX_WINDOWEDGE, "RabBit", settings::window_title.c_str(), style, left, top, width, height, NULL, NULL, GetModuleHandle(NULL), this);
-	// RB_ASSERT(_hwnd, "Cannot create window");
-
-	if (settings::fullscreen) {
-		DEVMODE dev_mode;
-		dev_mode.dmSize = sizeof(DEVMODE);
-		dev_mode.dmPelsWidth = width;
-		dev_mode.dmPelsHeight = height;
-		dev_mode.dmBitsPerPel = 32;
-		dev_mode.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT | DM_BITSPERPEL;
-
-		if (ChangeDisplaySettings(&dev_mode, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL) {
-			return;
-		}
-
-		// Make the window flags compatible with fullscreen mode
-		SetWindowLongPtr(_hwnd, GWL_STYLE, WS_POPUP | WS_CLIPCHILDREN | WS_CLIPSIBLINGS);
-		SetWindowLongPtr(_hwnd, GWL_EXSTYLE, WS_EX_APPWINDOW);
-
-		// Resize the window so that it fits the entire screen
-		SetWindowPos(_hwnd, HWND_TOP, 0, 0, width, height, SWP_FRAMECHANGED);
-		ShowWindow(_hwnd, SW_SHOW);
-	}
+	_hwnd = CreateWindow("RabBit", settings::window_title.c_str(), style,
+		left, top, width, height, NULL, NULL, GetModuleHandle(NULL), this);
+	RB_ASSERT(_hwnd, "Cannot create window");
 }
 
 window_win32::~window_win32() {
